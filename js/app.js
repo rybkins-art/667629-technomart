376af5da5ceb9d846e2bb745989305fca93e7dd9
Dmitry Rybakov <dmitry.rybakov@webpp.ru>
2018-03-29 18:33:02 +0300

    Поправил стили

--- js/app.js
+++ js/app.js
@@ -43,3 +43,24 @@ function addToBasket(evt) {
         $modal.classList.add("modal-visible");
     }
 }
+
+//Карта
+var yandexMap;
+
+// Дождёмся загрузки API и готовности DOM.
+ymaps.ready(init);
+
+function init () {
+    // Создание экземпляра карты и его привязка к контейнеру с
+    // заданным id ("map").
+    yandexMap = new ymaps.Map('ya-map', {
+        center: [59.938631, 30.323055],
+        zoom: 15,
+    }, {
+        searchControlProvider: 'yandex#search'
+    });
+    let myPlacemark = new ymaps.Placemark([59.938631, 30.323055], {
+        hintContent: 'ул. Б. Конюшенная, д. 19/8'
+    });
+    yandexMap.geoObjects.add(myPlacemark);
+}
